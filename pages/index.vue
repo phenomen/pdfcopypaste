1e5d8885dbbb2c809f48ce3a6785185424390020
Aleks <phenomen@pm.me>
2021-10-20 15:21:32 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,46 @@
 <template>
-  <div>
-    <Head lang="en-US">
-      <Title>PDF Copypaste</Title>
-      <Meta
-        name="description"
-        content="Easy way to fix PDF copypasting issues"
-      />
-    </Head>
-
-    <div class="container px-4 mx-auto">
-      <div class="hero min-h-screen">
-        <div class="text-center hero-content">
-          <div class="max-w-5xl">
-            <h1 class="mb-3 text-4xl uppercase font-bold">
-              PDF Copypaste
-            </h1>
-            <p class="mb-5 text-gray-500 text-sm">
-              An app to remove linebreaks and hyphenations when copypasting text
-              from PDF
-            </p>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Original Text</span>
-              </label>
-              <textarea
-                class="textarea h-64 textarea-primary textarea-bordered"
-                placeholder="Ctrl+V"
-                v-model="originalCopy"
-              ></textarea>
-            </div>
-
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Fixed Text</span>
-              </label>
-              <textarea
-                class="textarea h-64 textarea-success textarea-bordered"
-                placeholder="Ctrl+C"
-                @focus="$event.target.select()"
-                v-model="fixedCopy"
-              ></textarea>
-            </div>
-            <p class="mt-12 text-sm">
-              PDF Copypaste is an open source app by @Phenomen.
-              <a
-                class="link link-primary"
-                href="https://github.com/Phenomen/pdfcopypaste"
-                target="_blank"
-                >GitHub
-              </a>
-            </p>
+  <div class="container px-4 mx-auto">
+    <div class="hero min-h-screen">
+      <div class="text-center hero-content">
+        <div class="max-w-5xl">
+          <h1 class="mb-3 text-4xl uppercase font-bold">
+            PDF Copypaste
+          </h1>
+          <p class="mb-5 text-gray-500 text-sm">
+            An app to remove linebreaks and hyphenations when copypasting text
+            from PDF
+          </p>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Original Text</span>
+            </label>
+            <textarea
+              class="textarea h-64 textarea-primary textarea-bordered"
+              placeholder="Ctrl+V"
+              v-model="originalCopy"
+            ></textarea>
           </div>
+
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Fixed Text</span>
+            </label>
+            <textarea
+              class="textarea h-64 textarea-success textarea-bordered"
+              placeholder="Ctrl+C"
+              @focus="$event.target.select()"
+              >{{ fixedCopy }}</textarea
+            >
+          </div>
+          <p class="mt-12 text-sm">
+            PDF Copypaste is an open source app by @Phenomen.
+            <a
+              class="link link-primary"
+              href="https://github.com/Phenomen/pdfcopypaste"
+              target="_blank"
+              >GitHub
+            </a>
+          </p>
         </div>
       </div>
     </div>
@@ -65,7 +55,7 @@ export default {
     };
   },
   computed: {
-    fixedCopy() {
+    fixedCopy: function() {
       return this.originalCopy
         .replace(/(\r\n|\n|\r)/g, " ")
         .replaceAll("‑ ", "");
