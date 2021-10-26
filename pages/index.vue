f2c5598ffd871f463cce69190f545bd20e2b1a31
Aleks <phenomen@pm.me>
2021-10-26 10:02:33 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -32,29 +32,13 @@
               >{{ fixedCopy }}</textarea
             >
 
-            <div class="alert my-4">
-              <div class="flex-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="#2196f3"
-                  class="w-6 h-6 mx-2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  ></path>
-                </svg>
-                <button
-                  v-on:click="copyClipboard(fixedCopy)"
-                  class="btn btn-outline btn-wide"
-                >
-                  COPY TO CLIPBOARD
-                </button>
-              </div>
+            <div class="my-4">
+              <button
+                v-on:click="copyClipboard(fixedCopy)"
+                class="btn btn-outline btn-wide"
+              >
+                COPY TO CLIPBOARD
+              </button>
             </div>
           </div>
           <p class="mt-12 text-sm text-gray-500 ">
