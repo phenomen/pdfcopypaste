5e5f68bdbffe480d6ad9a1df2da22fdec323d6f4
Aleksandr Vasilenko <phenomen@proton.me>
2022-09-07 11:08:00 -0500

    Lighthouse contrast

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -68,6 +68,6 @@
 	<div class="mt-10 text-center text-xs text-slate-500 dark:text-slate-300">
 		Приложение <span class="font-medium">PDFCOPYPASTE</span> создано
 		<span class="font-medium">Phenomen#1337</span>. Исходный код доступен на
-		<a class="text-indigo-500 underline" href="https://github.com/phenomen/pdfcopypaste" target="_blank"> GitHub </a>
+		<a class="text-indigo-600 underline" href="https://github.com/phenomen/pdfcopypaste" target="_blank"> GitHub </a>
 	</div>
 </div>
