c3116682bd4d17357bfd4b89879661ea7eb1dd54
Aleksandr Vasilenko <phenomen@proton.me>
2023-03-17 12:06:55 -0500

    Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
